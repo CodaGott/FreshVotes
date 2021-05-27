181f0ca569d745b46f956278f1b24ff283865d95
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-05-27 01:09:53 +0100

    secuity update

--- src/main/java/com/freshvotes/controller/LoginController.java
+++ src/main/java/com/freshvotes/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.freshvotes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
